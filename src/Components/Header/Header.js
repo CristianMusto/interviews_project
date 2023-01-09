ea2a11ea36a8dae5fac6b09fc1a17fd6455ab254
Giacomo Boria <g.boria91@gmail.com>
2023-01-09 09:51:03 +0100

    add backgroud photo and change namelogo/home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,13 +4,15 @@ const Header = props => {
     return (
         <aside className={`${styles.sidebar} ${props.className}`}>
             <ul className={styles.voicesList}>
-               <li className={styles.logo}>LOGO</li>
+               <li className={styles.logo}>HOME</li>
                <li className={styles.whoarewe}>CHI SIAMO</li> 
                <li className={styles.course}>CORSI</li>
                <li className={styles.faq}>DOMANDE FREQUENTI</li>
                <li className={styles.contacts}>CONTATTI</li>
             </ul>
         </aside>
+
+        
     )
 }
 
