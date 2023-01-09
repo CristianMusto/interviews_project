fd726b28fa5d0ac6238e673c54b2ab4a076fc572
CristianMusto <cristian.musto00@gmail.com>
2023-01-09 12:13:56 +0100

    🔨start menubar *****

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -1,9 +1,11 @@
+import MenuBar from "../../UI/MenuBar/MenuBar"
 import TerminalComp from "../../UI/Terminal/Terminal"
 import styles from "./Home.module.scss"
 
 const Home = props => {
     return (
         <div className={styles.home}>
+            <MenuBar />
             <TerminalComp />
         </div>
     )
--- src/Components/UI/MenuBar/MenuBar.js
+++ src/Components/UI/MenuBar/MenuBar.js
@@ -1,9 +1,22 @@
 import styles from "./MenuBar.module.scss"
 
-const MenuBar = props => {
+const MenuBar = () => {
     return (
-        <div styles={`${styles.menubar} ${props.className}`}>
-            
+        <div className={styles.menubar}>
+            <ul className={styles.listMenuSx}>
+                <li>🍎</li>
+                <li><b>Terminale</b></li>
+                <li>Shell</li>
+                <li>Modifica</li>
+                <li>Vista</li>
+                <li>Finestra</li>
+                <li>Aiuto</li>
+            </ul>
+            <ul className={styles.listMenuDx}>
+                <li>🍎</li>
+                <li><b>Terminale</b></li>
+                <li>Shell</li>
+            </ul>
         </div>
     )
 }
